c9ad9eb04ff499751199e54bc1b618cab9d4215b
FatemehAlimoradi <f.alimoradi80@gmail.com>
2024-02-11 21:39:02 +0330

    fix pull() bugs

--- client.py
+++ client.py
@@ -1,19 +1,21 @@
+import base64
+import json
 import threading
 import time
-
 import requests
 
 
 class QueueClient:
-    def __init__(self, server_url):
+    def __init__(self, server_url, headers):
         self.server_url = server_url
+        self.headers = headers
         self.subscribe_func = None
 
     def push(self, key, value):
         url = f"{self.server_url}/push"
+        data = json.dumps({"key": key, "value": value})
 
-        data = {"queue_name": key, "value": value}
-        response = requests.post(url, json=data)
+        response = requests.request("POST", url, data=data, headers=self.headers)
 
         if response.status_code == 200:
             return "Message pushed successfully."
@@ -22,10 +24,18 @@ class QueueClient:
 
     def pull(self):
         url = f"{self.server_url}/pop"
-        response = requests.post(url, params={})
+
+        response = requests.post(url, params={}, headers=self.headers)
+
+        response_content = str(response.content)
+        response_values = json.loads(response_content[2:len(response_content) - 1])
 
         if response.status_code == 200:
-            return response.json()["key"], response.json()["value"]
+            if response_values['message'] == 'ok':
+                decoded_decimal = [byte for byte in base64.b64decode(response_values['value'])]
+                return response_values['key'], decoded_decimal
+            else:
+                return response_values['message']
         else:
             return "Failed to pop message."
 
@@ -43,6 +53,8 @@ class QueueClient:
 
 
 if __name__ == "__main__":
-    server_url = "http://localhost:8000"
-    client = QueueClient(server_url)
-    client.subscribe(None)
+    server_url = "http://87.247.170.145:8000"
+    client = QueueClient(server_url, {'Content-Type': 'application/json'})
+
+    print(client.push("testQueue26", [8, 10, 3, 5]))
+    print(client.pull())
