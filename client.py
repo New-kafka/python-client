52379d37efd05674abc99068001e414bae2119ec
FatemehAlimoradi <f.alimoradi80@gmail.com>
2024-02-11 22:39:01 +0330

    fix subscribe() bugs

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import base64
 import json
 import threading
 import time
+
 import requests
 
 
@@ -41,15 +42,22 @@ class QueueClient:
 
     def pull_periodically(self, f):
         while True:
-            key, value = self.pull()
-            if key and value:
-                f(key, value)
+            pull_result = self.pull()
+            if isinstance(pull_result, tuple):
+                key, value = pull_result
+                if key and value:
+                    f(key, value)
+
             time.sleep(1)
 
     def subscribe(self, f):
         self.subscribe_func = f
-        thread = threading.Thread(target=self.pull_periodically(f))
+        thread = threading.Thread(target=self.pull_periodically, args=(f,))
         thread.start()
+
+
+def func(key, value):
+    print("yes!")
 
 
 if __name__ == "__main__":
@@ -57,4 +65,16 @@ if __name__ == "__main__":
     client = QueueClient(server_url, {'Content-Type': 'application/json'})
 
     print(client.push("testQueue26", [8, 10, 3, 5]))
+    print(client.push("testQueue26", [8, 10, 3, 5]))
+    print(client.push("testQueue26", [8, 10, 3, 5]))
+    print(client.push("testQueue26", [8, 10, 3, 5]))
+    client.subscribe(func)
+    print(client.push("testQueue26", [8, 10, 3, 5]))
+    print(client.push("testQueue26", [8, 10, 3, 5]))
+    print(client.pull())
+    print(client.push("testQueue26", [8, 10, 3, 5]))
+    print(client.push("testQueue26", [8, 10, 3, 5]))
+    print(client.pull())
+    print(client.pull())
+    print(client.pull())
     print(client.pull())
